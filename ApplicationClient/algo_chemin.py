0c836e5c057f01a937a6a4b98f703fd9d84b6524
RomainTheobald <romaintheobald9pro@gmail.com>
2025-06-10 16:22:02 +0200

    ajout algo_chemin

---
+++ ApplicationClient/algo_chemin.py
@@ -0,0 +1,42 @@
+class File:
+    def init(self, n):
+        self.tab = []
+    def enfiler(self, x):
+        self.tab.append(x)
+    def defiler(self):
+        return self.tab.pop(0)
+    def est_vide(self):
+        return len(self.tab) == 0
+
+def dijkstra(graphe: dict, depart: str, bk_liste: list = []) -> dict:
+    """
+    Renvoie un dictionnaire avec pour chaque sommet accessible depuis 'depart' :
+    la distance minimale depuis 'depart'
+    le chemin emprunté (sous forme de liste de sommets)
+    :param graphe: dict des voisins {sommet: {voisin: distance, ...}, ...}
+    :param depart: sommet de départ
+    :param bk_liste: liste de sommets à éviter,vide par défaut
+    """
+    
+    file = File(len(graphe))
+    file.enfiler([depart])
+
+    dico = {depart: (0, [depart])}
+
+    while not file.est_vide():
+        chemin = file.defiler()
+        sommet = chemin[-1]
+        voisins = graphe.get(sommet, {})
+        for voisin, distance in voisins.items():
+            if voisin in bk_liste:
+                continue
+            nouvelle_distance = dico[sommet][0] + distance
+            nouveau_chemin = list(chemin) + [voisin]
+            if voisin not in dico:
+                dico[voisin] = (nouvelle_distance, nouveau_chemin)
+                file.enfiler(nouveau_chemin)
+            else:
+                if nouvelle_distance < dico[voisin][0]:
+                    dico[voisin] = (nouvelle_distance, nouveau_chemin)
+                    file.enfiler(nouveau_chemin)
+    return dico
