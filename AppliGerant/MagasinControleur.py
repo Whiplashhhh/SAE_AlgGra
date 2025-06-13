b8e38a35639f0d0de0c64a888bd8634412dbd0f6
Whiplashhhh <vanbaelinghemwillem@gmail.com>
2025-06-13 14:53:45 +0200

    ajouts commentaires appli 1

--- AppliGerant/MagasinControleur.py
+++ AppliGerant/MagasinControleur.py
@@ -13,27 +13,30 @@ class MagasinControleur:
 
     def get_produits_de_case(self, ligne, colonne):
         """
-        Donne à la vue la liste des produits, la clé case et la catégorie d'une case sélectionnée.
+        Fonction appelé quand une case est cliqué, va donner à MagasinVue, la case, les produits et la catégorie de la case cliquée
         """
-        if colonne < 26:
+        if colonne < 26:#On transforme la colonne en lettre
             lettre_colonne = chr(ord('A') + colonne)
         else:
             lettre_colonne = 'A' + chr(ord('A') + (colonne - 26))
         key = f"{ligne+1},{lettre_colonne}"
 
+        #Récupération des produits par catégorie
         categorie = self.modele.positions_categories.get(key, None)
         if categorie:
             produits = self.modele.produits_par_categories.get(categorie, [])
             return produits, key, categorie
         else:
             return [], key, None
-
+        
+    # Fonction pour ajouter un produits dans une catégorie
     def ajouter_produit(self, categorie, produit):
         self.modele.ajouter_produit(categorie, produit)
 
     def supprimer_produit(self, categorie, produit):
         self.modele.supprimer_produit(categorie, produit)
 
+    #Reinitialise tous les produits
     def reset_produits(self):
         self.modele.reset_produits()
 
