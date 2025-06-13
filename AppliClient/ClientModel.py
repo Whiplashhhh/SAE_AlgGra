a3126343260270ba5df796047fb9a3a44cb90350
Alex François <alexfrancois2604@gmail.com>
2025-06-13 21:13:28 +0200

    commentaire ClientModel

--- AppliClient/ClientModel.py
+++ AppliClient/ClientModel.py
@@ -8,6 +8,7 @@ class ClientModel:
     def __init__(self):
         self.charger_fichiers()
         self.liste_courses = {}
+        #Liste des caisses de fin de parcours
         self.caisses = [
             "44,AH", "44,AF", "44,AE", "44,AC", "44,AB",
             "44,Z", "44,Y", "44,W", "44,T", "44,S", "44,Q", "44,P", "44,O", "44,M"
@@ -19,6 +20,7 @@ class ClientModel:
 
         json_dir = os.path.abspath(json_dir)
 
+        #Ouverture des fichiers JSON nécessaires 
         with open(os.path.join(json_dir, "categorie.json"), "r", encoding="utf-8-sig") as f:
             self.sous_categories = json.load(f)
         with open(os.path.join(json_dir, "produits_par_categories_test.json"), "r", encoding="utf-8-sig") as f:
@@ -29,34 +31,40 @@ class ClientModel:
             self.graphe = json.load(f)
         self.categories = list(self.sous_categories.keys())
 
+    #Donne la liste de toutes les catgéories
     def get_categories(self):
         return self.categories
 
+    #Tous les produits d'une catégories 
     def get_produits_categorie(self, categorie):
         produits = []
         sous_cats = self.sous_categories.get(categorie, [])
         for sous_cat in sous_cats:
             produits.extend(self.produits_par_sous_categorie.get(sous_cat, []))
-        return sorted(set(produits))
+        return sorted(set(produits)) #Sert à obtenir une liste triée sans doublons
 
+    #Tous les produits du magasin
     def get_tous_les_produits(self):
         tous = []
         for cat in self.categories:
             tous.extend(self.get_produits_categorie(cat))
         return sorted(set(tous))
 
+    #Ajoute les produits à la liste de courses
     def ajouter_produit(self, produit):
         if produit in self.liste_courses:
             self.liste_courses[produit] += 1
         else:
             self.liste_courses[produit] = 1
 
+    #Retire les produits à la liste de courses
     def retirer_produit(self, produit):
         if produit in self.liste_courses:
             self.liste_courses[produit] -= 1
             if self.liste_courses[produit] <= 0:
                 del self.liste_courses[produit]
 
+    #Vide la liste de courses
     def effacer_liste(self):
         self.liste_courses = {}
 
@@ -72,7 +80,7 @@ class ClientModel:
         tout = self.get_tous_les_produits()
         tout = np.array(tout)
         n = min(10, len(tout))
-        indices = np.random.choice(len(tout), size=n, replace=False)
+        indices = np.random.choice(len(tout), size=n, replace=False)#Utilisation de numpy pour la liste aléatoire
         self.liste_courses = {}
         for i in indices:
             produit = tout[i]
@@ -81,8 +89,9 @@ class ClientModel:
     def get_liste_courses(self):
         return self.liste_courses
 
-    # ------------------ Chemin avec passage par sous-catégorie/positions ------------------
+    #Chemin avec passage par sous-catégorie/positions
 
+    #Donne les coordonnées des produits que l'on doit aller chercher dans le magasin
     def get_coords_produits(self, produits):
         coords = []
         produits_trouves = []
@@ -95,7 +104,7 @@ class ClientModel:
             if sous_cat_trouvee:
                 for coord, sous_cat in self.positions.items():
                     if coord not in self.graphe:
-                        continue  # <-- on ne garde QUE les cases accessibles
+                        continue  # On ignore les cases innaccssibles
                     if isinstance(sous_cat, list):
                         if sous_cat_trouvee in sous_cat:
                             coords.append(coord)
@@ -108,10 +117,10 @@ class ClientModel:
                             break
         return coords
 
-
+    #Algo de Dijkstra qui va permettre de calculer le chemin le plus court depuis un point donné
     def dijkstra(self, depart):
-        file = deque([[depart]])
-        dico = {depart: (0, [depart])}
+        file = deque([[depart]]) #Chemin à explorer
+        dico = {depart: (0, [depart])} #Dico gardant les distances
         while file:
             chemin = file.popleft()
             sommet = chemin[-1]
@@ -124,6 +133,7 @@ class ClientModel:
                     file.append(nouveau_chemin)
         return dico
 
+    #Va générer le chemin otpimal pour récupérer les produits jusqu'à la fin(les caisses)
     def get_chemin(self, depart, produits):
         coords_produits = self.get_coords_produits(produits)
         if not coords_produits:
@@ -131,6 +141,7 @@ class ClientModel:
         chemin_final = []
         position_actuelle = depart
         produits_restants = coords_produits.copy()
+        #On cherche ici le produit le plus proche à chaque fois
         while produits_restants:
             dico_chemin = self.dijkstra(position_actuelle)
             accessibles = [c for c in produits_restants if c in dico_chemin]
